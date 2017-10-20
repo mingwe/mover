947f6561d6783c4c1b96833c38aaf943789625c2
Peter Kolomiychenko <p.kolomiychenko@webversor.com>
2017-10-20 14:25:45 +0300

    st3

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
 // console.log(React);
 // console.log(ReactDOM);
 
+Date.prototype.daysInMonth = function() {
+    return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate();
+};
+
+var theDate = new Date();
+
+console.log(theDate);
+console.log(theDate.getMonth());
+console.log(theDate.daysInMonth());
+console.log(theDate.getDay());
+console.log(theDate);
+
+
+
 var mySlogans = [
     {
-        author: 'Simon',
-        text: 'Im doing breaking news'
-    },
-    {
         author: 'Blackie',
-        text: 'Wasp da best'
+        text: 'Wasp da best',
+        bigText: 'do you know what is like... heaven`s hung in black',
     },
     {
         author: 'Kurt',
-        text: 'sit and drink pennyroyal tea'
+        text: 'sit and drink pennyroyal tea',
+        bigText: 'Sit and drink Pennyroyal Tea\n' +
+        'Distill the life that\'s inside of me\n' +
+        'Sit and drink Pennyroyal Tea\n' +
+        'I\'m anemic royalty',
     },
     {
         author: 'Iggy',
-        text: 'its all messed up!'
+        text: 'its all messed up!',
+        bigText: 'its all messed up!... and now i wonna... so come on',
     }
 ];
 
@@ -33,27 +49,18 @@ var App = React.createClass({
     }
 });
 
-var Article = React.createClass({
-
-   render: function() {
-
-       var author = this.props.slogans.author,
-           text = this.props.slogans.text;
-
-       return (
-           <details>
-               <summary className="slogan__author">{author}:</summary>
-               <p className="slogan__text">{text}</p>
-           </details>
-       )
-   }
-});
 
 var Quotes = React.createClass({
+
+    propTypes: {
+        slogans: React.PropTypes.array.isRequired
+    },
+
     render: function() {
 
         var slogans = this.props.slogans;
         var slogansTemplate;
+
 
         if (slogans.length > 0) {
             slogansTemplate = slogans.map(function (item, index) {
@@ -74,9 +81,42 @@ var Quotes = React.createClass({
         return (
             <div className="qoutes">
                 {slogansTemplate}
-                <p className={slogans.length > 0 ? 'countEnabled':'hidden'}><strong>Total slogans: {slogans.length}</strong></p>
+                <p className={'slogans__cont ' + (slogans.length > 0 ? 'countEnabled':'hidden')}><strong>Total slogans: {slogans.length}</strong></p>
             </div>
         );
+    }
+});
+
+var Article = React.createClass({
+
+    propTypes: {
+        slogans: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+
+    getInitialState: function() {
+       return {
+           visible: false
+       };
+    },
+
+    render: function() {
+
+        var author = this.props.slogans.author,
+            text = this.props.slogans.text,
+            bigText = this.props.slogans.bigText;
+
+        return (
+            <details>
+                <summary className="slogan__author">{author}:</summary>
+                <p className="slogan__text">{text}</p>
+                <a href="#" className='slogan__readmore'>Read more...</a>
+                <p className="slogan__big-text">{bigText}</p>
+            </details>
+        )
     }
 });
 
