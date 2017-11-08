0229fcf28efa86bd6c27699d909dbef61d29f207
Peter Kolomiychenko <p.kolomiychenko@webversor.com>
2017-11-08 15:39:52 +0200

    mover v01

--- js/app3.js
+++ js/app3.js
@@ -1,11 +1,70 @@
+function checkCatch(plx, ply, aix, aiy, modificator) {
+    if ((((+plx)-(+modificator) < (+aix))) && (((+plx)+(+modificator)) > (+aix))) {
+        if ((((+ply)-(+modificator)) < (+aiy)) && (((+ply)+(+modificator)) > (+aiy))) {
+            console.log('CATCHED!');
+            return true
+        }
+    }
+    // console.log('------');
+    // console.log((+plx)-(+modificator));
+    // console.log((+aix));
+    // console.log((+plx)+(+modificator));
+}
+
 
 var App = React.createClass({
+
+    getInitialState: function () {
+        return {
+            enabled: true,
+            catched: false,
+            level: 2,
+            plposition: '0/0',
+            aiposition: '100/100',
+        }
+    },
+
+    componentWillUpdate: function () {
+        if (this.state.catched != true) {
+            let plx = (this.state.plposition).split('/')[0];
+            let aix = (this.state.aiposition).split('/')[0];
+            let ply = (this.state.plposition).split('/')[1];
+            let aiy = (this.state.aiposition).split('/')[1];
+            let modificator = 15;
+            if (checkCatch(plx, ply, aix, aiy, modificator)) {
+                this.setState({
+                    catched: true
+                })
+            }
+        }
+        console.log('upd');
+    },
+
+    position: function(value){
+        if (value[1]) {
+           this.setState({aiposition:value[0]});
+        }
+        else {
+           this.setState({plposition:value[0]});
+        }
+
+    },
+
     render: function() {
+        console.log(this.state.catched);
         return (
-        <div className={'parent-block'}>
-            <Mover name={'I AM MOVER'} speed={'5'}/>
-            <Mover name={'I AM MOVER'} speed={'2'} bot={true} changedirinterval={500}/>
-        </div>
+            <div>
+                {!this.state.catched &&
+                <div className={'parent-block'}>
+                    <Mover name={'WEBVERSOR'} speed={1000 / this.state.level} bot={false} position={this.position} stop={this.state.catched}/>
+                    <Mover name={''} speed={700 / this.state.level} bot={true} changedirinterval={2500}
+                           position={this.position} stop={this.state.catched}/>
+                </div>
+                }
+                {this.state.catched &&
+                    <div className={'gratz'}>WE DID IT!</div>
+                }
+            </div>
     );
     }
 });
@@ -54,7 +113,7 @@ function doMove (obj, dir) {
         }
     }
     obj.setState(prevState => ({
-        [direct]: prevState[direct] + 1*fwards
+        [direct]: prevState[direct] + 20*fwards
     }));
 }
 
@@ -76,10 +135,21 @@ function changeDir(obj, dir) {
 
 var Mover = React.createClass({
 
+    pickPosition: function(event) {
+        this.props.position([(this.state.left + '/' + this.state.up), this.props.bot]);
+    },
+
     getInitialState: function() {
+        let startPos
+        if (this.props.bot) {
+            startPos = 100;
+        }
+        else {
+            startPos = 0;
+        }
         return {
-            left: 0,
-            up: 0,
+            left: startPos,
+            up: startPos,
             direction: 'Right',
             windowWidth: window.innerWidth,
             windowHeight: window.innerHeight
@@ -87,14 +157,22 @@ var Mover = React.createClass({
     },
 
     componentDidMount() {
-        this.interval = setInterval(() => this.move(), this.props.speed);
+
         if (this.props.bot) {
             this.interval = setInterval(() => this.changeDirection(), this.props.changedirinterval);
         }
+        this.interval = setInterval(() => this.move(), this.props.speed);
+        console.log(this.props.speed);
+
     },
 
     move() {
         doMove(this, this.state.direction);
+        this.pickPosition();
+        if (this.props.stop) {
+            clearInterval(this.interval);
+            console.log('stopped');
+        }
     },
 
     changeDirection() {
@@ -112,14 +190,13 @@ var Mover = React.createClass({
 
     render: function() {
         return (
-            <div className={'mover'}
+            <div className={(!this.props.bot ? 'webversor ':'deadline ') + 'mover'}
                  tabIndex="-1" // Enables key handlers on div
                  onKeyDown={this.handleKeyPress} style={
                 {
-                    background: "#eee",
                     left: this.state.left + 'px',
                     top: this.state.up + 'px',
-                    margin: "50px"
+                    margin: "0px"
                 }
             }>
                 {this.props.name}
@@ -129,6 +206,9 @@ var Mover = React.createClass({
 });
 
 
-ReactDOM.render(
-    <App />,document.getElementById('root')
-);
+// ReactDOM.render(
+//     <App />,document.getElementById('root')
+// );
+
+
+var app = ReactDOM.render(<App/>, document.getElementById('root'));
